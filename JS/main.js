bd1c80e133c8878d44f341ad1e548d39d3b0561b
mohamed haithem <mohamedhyssm2019@gmail.com>
2023-01-26 09:22:32 +0200

    some issues fixed

--- JS/main.js
+++ JS/main.js
@@ -8,17 +8,14 @@ let firstFormSelect = document.querySelector(".container-fluid .form:nth-of-type
 transfromButton.onclick = getCurrency;
 
 async function getCurrency() {
-  let responseText = await fetch("https://api.currencyfreaks.com/latest?apikey=75cd6e99fe31446ca86cc0e8498e2c86"),
-    currency = await responseText.json()
-
-  if (+firstFormSelect.value === +secondFormSelect.value) {
-    secondFormSelect.value = firstFormSelect.value
-  } else if (firstFormSelect.value == 'USD' && secondFormSelect.value != 'USD') {
-    secondFormInput.value = (firstFormInput.value * currency.rates[secondFormSelect.value]).toFixed(3)
+  let currency = await fetch("https://api.currencyfreaks.com/latest?apikey=75cd6e99fe31446ca86cc0e8498e2c86"),
+    responseText = await currency.json()
+  if (firstFormSelect.value == 'USD' && secondFormSelect.value != 'USD') {
+    secondFormInput.value = (firstFormInput.value * responseText.rates[secondFormSelect.value]).toFixed(4)
   } else if (firstFormSelect.value != 'USD' && secondFormSelect.value == 'USD') {
-    secondFormInput.value = (firstFormInput.value / currency.rates[firstFormSelect.value]).toFixed(3)
+    secondFormInput.value = (firstFormInput.value / responseText.rates[firstFormSelect.value]).toFixed(4)
   } else {
-    let dollar = firstFormInput.value / currency.rates[firstFormSelect.value]
-    secondFormInput.value = (dollar * currency.rates[secondFormSelect.value]).toFixed(3)
+    let dollar = firstFormInput.value / responseText.rates[firstFormSelect.value]
+    secondFormInput.value = (dollar * responseText.rates[secondFormSelect.value]).toFixed(4)
   }
 }
